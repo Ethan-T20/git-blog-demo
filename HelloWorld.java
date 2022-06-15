b974a023c835855d14140ecc962bda4055f8e5b8
user <user@classroom.com>
2022-06-15 12:31:00 -0700

    Added HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/********************************************************************
+* Compilation: javac HelloWorld.java
+* Execution: java HelloWorld
+*
+*Prints "Hello, World". By tradition this is everyone's first program.
+*
+*%java HelloWorld
+*Hello, World
+*
+*These 17 lines of text are comments.
+*********************************************************************/
+
+public class HelloWorld{
+
+	public static void main(String[] args){
+	//prints "Hello, World".
+	System.out.println("Hello, World");
+	}
+
+	}
